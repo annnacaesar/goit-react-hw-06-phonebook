809bc508f50e901e0430e205866fc6ecfba3e2f8
annnacaesar <Anechka22334@gmail.com>
2022-08-07 10:38:19 +0300

    initial settings

---
+++ src/components/App/App.jsx
@@ -0,0 +1,90 @@
+import { useState, useEffect} from 'react';
+import { nanoid } from 'nanoid';
+import ContactList from '../ContactList';
+import ContactForm from '../ContactForm';
+import Filter from '../Filter';
+import { AppContainer, Container, TitleMain, Title } from './App.styled';
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+
+export const App = () => {
+	const [contacts, setContacts] = useState(
+		() => JSON.parse(window.localStorage.getItem('contacts')) ?? []
+	);
+	const [filter, setFilter] = useState('');
+
+	const deleteContact = id => {
+		setContacts(contacts.filter(contact => contact.id !== id));
+	};
+
+	const addContact = (name, number) => {
+		const contact = {
+			id: nanoid(),
+			name,
+			number,
+		};
+
+		const findContact = contacts.find(contact =>
+			contact.name.toLowerCase().includes(name.toLowerCase())
+		);
+
+		findContact
+			? toast.info(`${name} is already in contact`)
+			: setContacts([contact, ...contacts]);
+	};
+
+	const changeFilter = event => {
+		setFilter(event.currentTarget.value);
+	};
+	const isVisibleContacts = () => {
+		const normalizeFilter = filter.toLowerCase();
+
+		if (contacts.length !== 0) {
+			return contacts.filter(contact =>
+				contact.name.toLowerCase().includes(normalizeFilter)
+			);
+		}
+		return;
+	};
+
+	useEffect(() => {
+		const contacts = window.localStorage.getItem('contacts');
+		const parsedContacts = JSON.parse(contacts);
+
+		if (parsedContacts) setContacts(parsedContacts);
+	}, []);
+
+	useEffect(() => {
+		window.localStorage.setItem('contacts', JSON.stringify(contacts));
+	}, [contacts]);
+
+	return (
+		<AppContainer>
+			<ToastContainer
+				position="top-center"
+				autoClose={5000}
+				theme="dark"
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+			/>
+			<Container>
+				<TitleMain>Phonebook</TitleMain>
+				<ContactForm onSubmit={addContact} />
+			</Container>
+			<Container>
+				<Title>Contacts</Title>
+				<Filter filter={filter} onChange={changeFilter} />
+				<ContactList
+					contacts={isVisibleContacts()}
+					onDeleteContact={deleteContact}
+				/>
+			</Container>
+		</AppContainer>
+	);
+};
